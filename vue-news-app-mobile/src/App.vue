447470361ce4c70f82a3019e177c3276a7076a2e
QianGuoqing <qian_guoqing@yeah.net>
2018-01-03 21:25:45 +0800

    给移动端news-app添加新闻导航栏

--- vue-news-app-mobile/src/App.vue
+++ vue-news-app-mobile/src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
     <app-header></app-header>
+    <!-- 新闻tab栏 -->
+    <el-tabs v-model="currentTab" type="card">
+      <el-tab-pane label="头条" name="top"></el-tab-pane>
+      <el-tab-pane label="社会" name="shehui"></el-tab-pane>
+      <el-tab-pane label="国内" name="guonei"></el-tab-pane>
+      <el-tab-pane label="国际" name="guoji"></el-tab-pane>
+      <el-tab-pane label="娱乐" name="yule"></el-tab-pane>
+    </el-tabs>
     <app-footer></app-footer>
   </div>
 </template>
@@ -14,7 +22,12 @@ export default {
   components: {
     'app-header': Header,
     'app-footer': Footer
-  }
+  },
+  data() {
+    return {
+      currentTab: 'top'
+    }
+  },
 }
 </script>
 
