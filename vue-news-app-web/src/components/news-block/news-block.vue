a29882ef43a86ff9eb0faf7600a843bc33c2ba55
QianGuoqing <qian_guoqing@yeah.net>
2018-01-05 10:40:15 +0800

    使用axios获取新闻信息，并在new-block组件中显示各个部分新闻列表

--- vue-news-app-web/src/components/news-block/news-block.vue
+++ vue-news-app-web/src/components/news-block/news-block.vue
@@ -1,15 +1,51 @@
 <template>
-  <div>
-
+  <div class="news-block">
+    <Card>
+      <ul class="news-block-list">
+        <li v-for="item in newsList" :key="item.uniquekey" class="news-block-list-item">
+          <a href="" target="_blank">{{ item.title }}</a>
+        </li>
+      </ul>
+    </Card>
   </div>
 </template>
 
 <script>
+
+  import axios from 'axios'
+
   export default {
-    
+    data() {
+      return {
+        newsList: ''
+      }
+    },
+    props: {
+      newsType: {
+        type: String,
+        default: ''
+      },
+      newsCount: {
+        type: Number,
+        default: 0
+      }
+    },
+    created() {
+      this.$nextTick(() => {
+        axios.get(`http://newsapi.gugujiankong.com/Handler.ashx?action=getnews&type=${this.newsType}&count=${this.newsCount}`).then(response => {
+          response = response.data
+          this.newsList = response
+        }).catch(error => {
+          console.log(error)
+        })
+      })
+    }
   }
 </script>
 
 <style scoped>
-
+.news-block-list-item a {
+  display: inline-block;
+  line-height: 2.5;
+}
 </style>
--- vue-news-app-web/src/components/news-container/news-container.vue
+++ vue-news-app-web/src/components/news-container/news-container.vue
@@ -2,12 +2,28 @@
   <div class="news-container">
     <Row>
       <Col span="2" class="space">.</Col>
-      <Col span="20" class="container">
+      <Col span="20" class="news-content-container">
         <div class="left-container">
           <div class="carousel">
             <!-- 轮播图组件 -->
             <news-carousel></news-carousel>
           </div>
+        </div>
+        <div class="center-container">
+          <Tabs class="news-block-tabs">
+            <TabPane label="头条" name="top">
+              <news-block news-type="top" :news-count="20"></news-block>
+            </TabPane>
+            <TabPane label="国际" name="guoji">
+              <news-block news-type="guoji" :news-count="20"></news-block>
+            </TabPane>
+            <TabPane label="国内" name="guonei">
+              <news-block news-type="guonei" :news-count="20"></news-block>
+            </TabPane>
+          </Tabs>
+        </div>
+        <div class="right-container">
+
         </div>
       </Col>
       <Col span="2" class="space">.</Col>
@@ -18,10 +34,12 @@
 <script>
 
   import NewsCarousel from '../news-carousel/news-carousel.vue'
+  import NewsBlock from '../news-block/news-block.vue'
 
   export default {
     components: {
-      NewsCarousel
+      NewsCarousel,
+      NewsBlock
     }
   }
 </script>
@@ -32,8 +50,15 @@
   display: flex;
 }
 
+.news-content-container {
+  display: flex;
+}
+
 .left-container {
   width: 400px;
 }
 
+.news-block-tabs {
+  padding-left: 20px;
+}
 </style>
