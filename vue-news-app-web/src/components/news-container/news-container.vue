ae2d5e2845ad44b193fd96ab4b25bb657a102bf4
QianGuoqing <qian_guoqing@yeah.net>
2018-01-06 10:09:41 +0800

    添加新闻图片组件

--- vue-news-app-web/src/components/news-container/news-container.vue
+++ vue-news-app-web/src/components/news-container/news-container.vue
@@ -8,23 +8,55 @@
             <!-- 轮播图组件 -->
             <news-carousel></news-carousel>
           </div>
+          <div class="news-image-block-container">
+            <news-image-block 
+              width="400px" 
+              card-title="国际头条"
+              image-type="guoji"
+              :image-count="6"
+              :image-width="112"></news-image-block>
+          </div>
         </div>
         <div class="center-container">
           <Tabs class="news-block-tabs">
             <TabPane label="头条" name="top">
-              <news-block news-type="top" :news-count="20"></news-block>
+              <news-block news-type="top" :news-count="17"></news-block>
             </TabPane>
             <TabPane label="国际" name="guoji">
-              <news-block news-type="guoji" :news-count="20"></news-block>
+              <news-block news-type="guoji" :news-count="17"></news-block>
             </TabPane>
             <TabPane label="国内" name="guonei">
-              <news-block news-type="guonei" :news-count="20"></news-block>
+              <news-block news-type="guonei" :news-count="17"></news-block>
             </TabPane>
           </Tabs>
         </div>
         <div class="right-container">
 
         </div>
+      </Col>
+      <Col span="2" class="space">.</Col>
+    </Row>
+    <Row>
+      <Col span="2" class="space">.</Col>
+      <Col span="20">
+        <news-image-block
+          width="100%"
+          :image-count="7"
+          image-type="guonei"
+          card-title="国内新闻"
+          image-width="150"></news-image-block>
+      </Col>
+      <Col span="2" class="space">.</Col>
+    </Row>
+    <Row>
+      <Col span="2" class="space">.</Col>
+      <Col span="20">
+        <news-image-block
+          width="100%"
+          :image-count="14"
+          image-type="yule"
+          card-title="娱乐新闻"
+          image-width="160"></news-image-block>
       </Col>
       <Col span="2" class="space">.</Col>
     </Row>
@@ -35,11 +67,13 @@
 
   import NewsCarousel from '../news-carousel/news-carousel.vue'
   import NewsBlock from '../news-block/news-block.vue'
+  import NewsImageBlock from '../news-image-block/news-image-block.vue'
 
   export default {
     components: {
       NewsCarousel,
-      NewsBlock
+      NewsBlock,
+      NewsImageBlock
     }
   }
 </script>
@@ -47,7 +81,6 @@
 <style scoped>
 .news-container {
   margin-top: 30px;
-  display: flex;
 }
 
 .news-content-container {
--- vue-news-app-web/src/components/news-image-block/news-image-block.vue
+++ vue-news-app-web/src/components/news-image-block/news-image-block.vue
@@ -1,15 +1,86 @@
 <template>
-  <div>
-
+  <div class="image-block">
+    <Card :style="{width: width}">
+      <p slot="title">{{ cardTitle }}</p>
+      <ul class="news-list">
+        <li v-for="news in newsList" :key="news.uniquekey">
+          <div class="image-item-block">
+            <div class="custom-image">
+              <img :style="{width: imageWidth + 'px'}" :src="news.thumbnail_pic_s" alt="">
+            </div>
+            <div class="custom-description">
+              <h3 class="news-title">{{ news.title }}</h3>
+              <p class="author-name">{{ news.author_name }}</p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </Card>
   </div>
 </template>
 
 <script>
+
+  import axios from 'axios'
+
   export default {
-    
+    data () {
+      return {
+        newsList: ''
+      }
+    },
+    props: {
+      imageWidth: {
+        type: Number,
+        default: 0
+      },
+      imageCount: {
+        type: Number,
+        default: 0
+      },
+      imageType: {
+        type: String,
+        default: ''
+      },
+      width: {
+        type: String,
+        default: 0
+      },
+      cardTitle: {
+        type: String,
+        default: ''
+      }
+    },
+    created() {
+      this.$nextTick(() => {
+        axios.get(`http://newsapi.gugujiankong.com/Handler.ashx?action=getnews&type=${this.imageType}&count=${this.imageCount}`).then(response => {
+          response = response.data
+          this.newsList = response
+        }).catch(error => {
+          console.log(error)
+        })
+      })
+    }
   }
 </script>
 
 <style scoped>
+.news-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
 
+.news-title {
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  width: 110px;
+}
+
+.author-name {
+  padding: 2px;
+  font-size: 12px;
+  color: #aaa;
+}
 </style>
